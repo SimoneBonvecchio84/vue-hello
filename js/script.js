bee1b32c3a764a859221b3f585dd1be5caf68bb6
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-04-17 16:03:01 +0200

    Feat: Add imput name and lastName, and print function to print message into html

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 //Scaffolding per utilizzo di Vue js
-
+console.log(Vue);
 const { createApp } = Vue;
 
 createApp({
     data() {
-
+        return{
+            userMessage:"Benvenuto in Vue Js",
+            user: "",
+            lastName: "",
+            image: "./../img/baffi.jpg",
+        }
     },
     methods: {
+        print: function () {
+           this.userMessage =`Benvenuto in Vue Js ${this.user} ${this.lastName}`; 
 
-    }
+            this.user = "";
+            this.lastName = "";
+        },
+    },
 }).mount("#app");
